7008b9a8b67e0a5a4184f334d9baf7e5a519a99f
Brendan Douglas <brendan.douglas@act.gov.au>
2021-08-26 20:59:11 +1000

    Create practitioner identifier enum

---
+++ pegacorn-buildingblocks-datamodels/pegacorn-buildingblocks-datamodels-ldap/src/main/java/net/fhirfactory/pegacorn/buildingblocks/datamodels/ldap/PractitionerIdentifierTypes.java
@@ -0,0 +1,15 @@
+package net.fhirfactory.pegacorn.buildingblocks.datamodels.ldap;
+
+/**
+ * An enum of practitioner identifiers.
+ * 
+ * @author brendan_douglas_a
+ *
+ */
+public enum PractitionerIdentifierTypes {
+	AGS,
+	GS1,
+	IRN,
+	COMMON_NAME,
+	ACCOUNT_NAME;
+}
