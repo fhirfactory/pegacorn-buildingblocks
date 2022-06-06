692543323dd1bef117e5c725253d4bb2f6397204
kelskye <kelskye@gmail.com>
2022-06-06 16:59:03 +1000

    better naming of package

--- datamodels/core/src/main/java/net/fhirfactory/pegacorn/core/interfaces/media/PetasosMediaServiceAgentInterface.java
+++ datamodels/core/src/main/java/net/fhirfactory/pegacorn/core/interfaces/media/PetasosMediaServiceAgentInterface.java
@@ -19,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package net.fhirfactory.pegacorn.core.interfaces.datawarehouse;
+package net.fhirfactory.pegacorn.core.interfaces.media;
 
 import org.hl7.fhir.r4.model.Media;
 
