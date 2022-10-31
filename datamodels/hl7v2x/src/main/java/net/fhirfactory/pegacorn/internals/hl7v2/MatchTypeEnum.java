8e46cdbe0256ca4f76d1d7b25f253f57907ef8d0
Terry O'Neill <terry.oneill@act.gov.au>
2022-10-31 14:28:28 +1100

    #272203 - Added compare option for equal ignoring case (and not)

--- datamodels/hl7v2x/src/main/java/net/fhirfactory/pegacorn/internals/hl7v2/MatchTypeEnum.java
+++ datamodels/hl7v2x/src/main/java/net/fhirfactory/pegacorn/internals/hl7v2/MatchTypeEnum.java
@@ -8,13 +8,15 @@ package net.fhirfactory.pegacorn.internals.hl7v2;
  */
 public enum MatchTypeEnum {
 	EQUALS("equals"),
+	EQUALS_IGNORE_CASE("equals-ignore-case"),
 	CONTAINS("contains"),
 	STARTS_WITH("starts-with"),
 	ENDS_WITH("ends-with"),
 	NOT_EQUALS("not-equals"),
+	NOT_EQUALS_IGNORE_CASE("not-equals-ignore-case"),
 	NOT_CONTAINS("not-contains"),
 	NOT_STARTS_WITH("not-starts-with"),
-	NOT_ENDS_WITH("not-ends-with");
+	NOT_ENDS_WITH("not-ends-with");;
 	
 	private String type;
 	
